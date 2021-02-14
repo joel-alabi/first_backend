2826cc814c03db262f0aa79c4a933cd65441f9e4
Joel Alabi <joelalabi45@gmail.com>
2021-02-14 17:10:22 +0000

    started express

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const http = require('http');
+const express = require('express');
 
 const handleAllRequest = (requestObject,responseObject) =>{
     console.log('hello i recicieved a request')
-     const url = requestObject.url;
-     responseObject.setHeader('content-type','text/html');
-    if (url === '/'){
-        responseObject.write('<h1>Welcome to homepage</h1>')
-    }else if(url ==='/login'){
-        responseObject.write('<h1>this is login page</h1>')
-    }else if(url ==='/signup'){
-        responseObject.write('<h1>this is signup page</h1>')
-    }else if(url ==='/profile'){
-        responseObject.write('<h1>this is profile page</h1>')
-    }
-    else{
-        responseObject.write('<h1>server shut down</h1>')
-    }
-   responseObject.end();
+     const url = requestObject.url;    
+     responseObject.write('<h1>server shut down</h1>')
+    
+     responseObject.end();
 }
 
+const handleHomeRequest =(req,res)=>{
+    res.send("<h1>Welcome to homepage</h1>")
+}
+const handleLoginRequest =(req,res)=>{
+    res.send("<h1>this is login post</h1><h3>started nodejs with express very fun</h3><button> submit</button>")
+}
+const handleSignupRequest =(req,res)=>{
+    res.send("<h1>this is signup page</h1>")
+}
+const handleProfileRequest =(req,res)=>{
+    res.send("<h1>this is profile page</h1>")
+}
+ 
 
-const server = http.createServer(handleAllRequest);
+const server = express();
+server.put('/profile',handleProfileRequest)
+server.get('/login',handleLoginRequest)
+server.post('/login',handleLoginRequest)
+server.use('/signup',handleSignupRequest)
+server.use("/",handleHomeRequest);
+
 
 server.listen(3000,'127.0.0.1',()=>
 console.log('server is running')
